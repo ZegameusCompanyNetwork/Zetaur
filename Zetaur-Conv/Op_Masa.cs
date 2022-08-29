e5fdee0cc21bbb0952d036b911c7c220e1393445
Zegameus YT <30315974+Zegameus@users.noreply.github.com>
2022-08-30 00:41:05 +0200

    Upload new files
    These files change the entire program's form and design.

---
+++ Zetaur-Conv/Op_Masa.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace Zetaur_Conv
+{
+    /// <summary>
+    /// Accede a los métodos de conversión de unidades de masa.
+    /// <list type="bullet">
+    /// <item><description>Kilogramos</description></item>
+    /// <item><description>Gramos</description></item>
+    /// <item><description>Toneladas (métricas)</description></item>
+    /// <item><description>Toneladas Cortas (Us t)</description></item>
+    /// <item><description>Toneladas Largas (Uk t)</description></item>
+    /// <item><description>Onzas</description></item>
+    /// <item><description>Libras</description></item>
+    /// <item><description>Stones</description></item>
+    /// </list>
+    /// </summary>
+    public class Op_Masa
+    {
+        /// <summary>
+        /// 0 = Kg, 1 = g, 2 = t, 3 = US t, 4 = UK t, 5 = Oz, 6 = Lb, 7 = St
+        /// </summary>
+        static readonly string[] masas = { "Kilogramos (kg)", "Gramos (g)", "Toneladas (t)", "Tonelada Corta (US t)", "Tonelada larga (UK t)", "Onzas (Oz)", "Libras (lb)", "Stones (st)" };
+        #region Kilogramos-Gramos
+        /// <summary>
+        /// Transforma Kilogramos al resto de unidades <see cref="Op_Masa"/>
+        /// </summary>
+        /// <param name="i">Kg es un número double.</param>
+        /// 
+        /// <returns>Array con los valores convertidos (g, t, US t, Uk t, Oz, Lb, St)</returns>
+        public static double[] OpKg(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i * 1000; //g
+            o[1] = i / 1000;//t
+            o[2] = i /907.185;//UsT
+            o[3] = i / 1016;//Uk t
+            o[4] = i * 35.274;//Oz
+            o[5] = i * 2.20462;//Lb
+            o[6] = i * 6.35029318;//St
+            return o;
+        }
+        /// <summary>
+        /// Transforma Gramos (g) al resto de unidades <see cref="Op_Masa"/>
+        /// </summary>
+        /// <param name="i">g es un número double </param>
+        /// 
+        /// <returns>Array con los valores convertidos (Kg, t, US t, Uk t, Oz, Lb, St)</returns>
+        public static double[] OpG(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i * 1000; //kg
+            o[1] = i * Math.Pow(10, -6);//t
+            o[2] = i / 907185;//UsT
+            o[3] = i / (1.016 * Math.Pow(10, 6));//Uk t
+            o[4] = i / 28.3495;//Oz
+            o[5] = i / 453.592;//Lb
+            o[6] = i / 6350.29318;//St
+            return o;
+        }
+        #endregion
+        #region Toneladas
+        /// <summary>
+        /// Transforma toneladas (métricas) al resto de unidades <see cref="Op_Masa"/>.
+        /// </summary>
+        /// <param name="i">T es un número double.</param>
+        /// <returns>Array con los valores convertidos (Kg, g, US t, Uk t, Oz, Lb, St)</returns>
+        public static double[] OpT(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i * 1000; //kg
+            o[1] = i * Math.Pow(10, 6);//g
+            o[2] = i * 1.10231;//UsT
+            o[3] = i / 1.016;//Uk t
+            o[4] = i * 35274;//Oz
+            o[5] = i * 2204.62;//Lb
+            o[6] = i * 157;//St
+            return o;
+        }
+        /// <summary>
+        /// Transforma toneladas cortas (Us t) al resto de unidades <see cref="Op_Masa"/>.
+        /// </summary>
+        /// <param name="i">UsT es un número double.</param>
+        /// <returns>Array con los valores convertidos (Kg, g, t, Uk t, Oz, Lb, St)</returns>
+        public static double[] OpUsT(double i)
+        {
+            double[] o = new double[7];
+
+            o[0] = i * 907.185;
+            o[1] = i * 907185;
+            o[2] = i / 1.10231;
+            o[3] = i / 1.12;
+            o[4] = i * 32000;
+            o[5] = i * 2000;
+            o[6] = i * 142.857;
+            return o;
+        }
+        /// <summary>
+        /// Transforma Toneladas Largas (Uk T) al resto de unidades <see cref="Op_Masa"/>
+        /// </summary>
+        /// <param name="i">UkT es un número double.</param>
+        /// <returns>Array con los valores convertidos (Kg, g, t, US t, Oz, Lb, St)</returns>
+        public static double[] OpUkT(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i * 1026;//Kg
+            o[1] = i * 1.016 * Math.Pow(10, -6);//g
+            o[2] = i * 1.016;//T
+            o[3] = i * 1.12;//US t
+            o[4] = i * 35840;//Oz
+            o[5] = i * 2240;//Lb
+            o[6] = i * 160;//St
+            return o;
+        }
+        #endregion
+        #region Libras-Onzas-Stones
+        /// <summary>
+        /// Transforma Onzas (Oz) al resto de unidades <see cref="Op_Masa"/>.
+        /// </summary>
+        /// <param name="i">Oz es un número double.</param>
+        /// <returns>Array con los valores convertidos (Kg, g, t, US t, Uk t, Lb, St)</returns>
+        public static double[] OpOz(double i)
+        {
+            double[] o = new double[7];
+
+            o[0] = i / 35.274; //Kg
+            o[1] = i * 28.3495; //g
+            o[2] = i / 35274; //t
+            o[3] = i / 35840;//US t
+            o[4] = i / 32000;//Uk t
+            o[5] = i / 16; //Lb
+            o[6] = i / 224;//St
+            return o;
+        }
+        /// <summary>
+        /// Transforma Libras (Lb) al resto de unidades <see cref="Op_Masa"/>.
+        /// </summary>
+        /// <param name="i">Lb es un número double.</param>
+        /// <returns>Array con los valores convertidos (Kg, g, t, US t, Uk t, Oz, St)</returns>
+        public static double[] OpLb(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i / 2.205;//Kg
+            o[1] = i / 454;//g
+            o[2] = i / 2205;//t
+            o[3] = i / 2240;//US t
+            o[4] = i / 2000;//Uk t
+            o[5] = i * 16;//Oz
+            o[6] = i / 14;//St
+            return o;
+        }
+        /// <summary>
+        /// Transforma Stones (St) al resto de unidades <see cref="Op_Masa"/>.
+        /// </summary>
+        /// <param name="i">St es un número double.</param>
+        /// <returns>Array con los valores convertidos (Kg, g, t, US t, Uk t, Oz, Lb)</returns>
+        public static double[] OpSt(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i * 6.35029; //Kg
+            o[1] = i * 6350.29; //g
+            o[2] = i / 157; //t
+            o[3] = i / 160; //US t
+            o[4] = i / 143; //Uk t
+            o[5] = i * 224; //Oz
+            o[6] = i * 14; //Lb
+            return o;
+        }
+        #endregion
+    }
+}
---
+++ Zetaur-Conv/Op_Presion.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace Zetaur_Conv
+{
+    /// <summary>
+    /// Clase con los métodos de transformación de unidades de presión.
+    /// <list type="table">
+    /// <listheader><term>Unidad</term><description>Nombre de completo de unidad</description></listheader>
+    /// <item><term>atm</term><description>Atmósferas Técnicas</description></item>
+    /// <item><term>bar</term><description>Bares</description></item>
+    /// <item><term>mbar</term><description>Milibares</description></item>
+    /// <item><term>PSI</term><description>Libras por pulgada cuadrada</description></item>
+    /// <item><term>Pa</term><description>Pascales</description></item>
+    /// <item><term>hPa</term><description>Hectopascales</description></item>
+    /// <item><term>mmHg</term><description>Milímetros de Mercurio</description></item>
+    /// <item><term>torr</term><description>Torr</description></item>
+    /// </list>
+    /// </summary>
+    public class Op_Presion
+    {
+        /// <summary>
+        /// 0 = Atm, 1 = bar, 2 = mbar, 3 = PSI, 4 = Pa, 5 = hPa, 6 = mmHg, 7 = Torr, 8 = kp/cm2
+        /// </summary>
+        static readonly string[] p = { "Atmósferas (atm)", "Bares (bar)", "Milibares (mbar)", "Libra por pulgada cuadrada (PSI)", "Pascales (Pa)", "Hectopascales (hPa)", "Milímetros de Mercurio (mmHg)", "Torr (torr)", "Kilopondio por centímetro cuadrado" };
+
+        /// <summary>
+        /// Convierte Atmosferas al resto de unidades.
+        /// </summary>
+        /// <param name="i">Valor a convertir</param>
+        /// <returns>Array con los valores convertidos (Bar, mBar, PSI, Pa, hPa, mmHg, kpcm2)</returns>
+        public static double[] OpAtm(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i * 1.01325;//Bar
+            o[1] = i * 1013.25;//mBar
+            o[2] = i * 14.6959;//PSI
+            o[3] = i * 101325;//Pa
+            o[4] = i * 1013.25;//hPa
+            o[5] = i * 760; //mmHg
+            o[6] = i * 1.03323;//kpcm2
+            return o;
+        }
+
+        /// <summary>
+        /// Convierte Bares al resto de unidades.
+        /// </summary>
+        /// <param name="i">Valor a convertir</param>
+        /// <returns>Array con los valores convertidos (Atm, mBar, PSI, Pa, hPa, mmHg, kpcm2)</returns>
+        public static double[] Bar(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i / 1.01325;//Atm
+            o[1] = i * 1000;//mBar
+            o[2] = i * 14.5038;//PSI
+            o[3] = i * 100000;//Pa
+            o[4] = i * 1000;//hPa
+            o[5] = i * 750.062; //mmHg
+            o[6] = i * 1.01972;//kpcm2
+            return o;
+        }
+
+        /// <summary>
+        /// Convierte mílibares al resto de unidades
+        /// </summary>
+        /// <param name="i">Valor a convertir</param>
+        /// <returns>Array con los valores convertidos (Atm, Bar, PSI, Pa, hPa, mmHg, kpcm2)</returns>
+        public static double[] mBar(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i / 1013.25;//Atm
+            o[1] = i / 1000;//Bar
+            o[2] = i / 68.9476;//PSI
+            o[3] = i * 100;//Pa
+            o[4] = i;//hPa
+            o[5] = i / 1.33322; //mmHg
+            o[6] = i / 980.665;//kpcm2
+            return o;
+        }
+
+        /// <summary>
+        /// Convierte Libras por pulgada cuadrada al resto de unidades
+        /// </summary>
+        /// <param name="i">Valor a convertir</param>
+        /// <returns>Array con los valores convertidos (Atm, Bar, mBar, Pa, hPa, mmHg, kpcm2)</returns>
+        public static double[] PSI(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i * 1.01325;//Atm//Bar
+            o[1] = i * 1013.25;//mBar
+            o[2] = i * 14.6959;
+            o[3] = i * 101325;//Pa
+            o[4] = i * 1013.25;//hPa
+            o[5] = i * 760; //mmHg
+            o[6] = i * 1.03323;//kpcm2
+            return o;
+        }
+
+        /// <summary>
+        /// Convierte Pascales al resto de unidades
+        /// </summary>
+        /// <param name="i">Valor a convertir</param>
+        /// <returns>Array con los valores convertidos (Atm, Bar, mBar, PSI, hPa, mmHg, kpcm2)</returns>
+        public static double[] Pa(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i * 1.01325;//Atm//Bar
+            o[1] = i * 1013.25;//mBar
+            o[2] = i * 14.6959;//PSI
+            o[3] = i * 101325;//Pa
+            o[4] = i * 1013.25;//hPa
+            o[5] = i * 760; //mmHg
+            o[6] = i * 1.03323;//kpcm2
+            return o;
+        }
+
+        /// <summary>
+        /// Convierte HectoPascales al resto de unidades
+        /// </summary>
+        /// <param name="i">Valor a convertir</param>
+        /// <returns>Array con los valores convertidos (Atm, Bar, mBar, PSI, Pa, mmHg, kpcm2)</returns>
+        public static double[] hPa(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i * 1.01325;//Atm//Bar
+            o[1] = i * 1013.25;//mBar
+            o[2] = i * 14.6959;//PSI
+            o[3] = i * 101325;//Pa
+            o[4] = i * 1013.25;//hPa
+            o[5] = i * 760; //mmHg
+            o[6] = i * 1.03323;//kpcm2
+            return o;
+        }
+
+        /// <summary>
+        /// Convierte miligramos de Mercurio al resto de unidades
+        /// </summary>
+        /// <param name="i">Valor a convertir</param>
+        /// <returns>Array con los valores convertidos (Atm, Bar, mBar, PSI, Pa, hPa, mmHg, kpcm2)</returns>
+        public static double[] mmHgTorr(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i / 760;//Atm
+            o[1] = i / 750.062;//Bar
+            o[2] = i * 1.33322;//mbar
+            o[3] = i / 51.715;//PSI
+            o[4] = i * 133;//Pa
+            o[5] = i * 1.33; //hPa
+            o[6] = i / 735.559;//kpcm2
+            return o;
+        }
+
+        /// <summary>
+        /// Convierte Kilopondios por centímetro cuadrado al resto de unidades
+        /// </summary>
+        /// <param name="i">Valor a convertir</param>
+        /// <returns>Array con los valores convertidos (Atm, Bar, mBar, PSI, Pa, hPa, mmHg)</returns>
+        public static double[] kpcm2(double i)
+        {
+            double[] o = new double[7];
+            o[0] = i / 1.033;//Atm
+            o[1] = i / 1.01972;//Bar
+            o[2] = i * 1019.72;//mBar
+            o[3] = i * 14.223;//PSI
+            o[4] = i * 98067;//Pa
+            o[5] = i * 980.67; //hPa
+            o[6] = i * 735.559;//mmHg
+            return o;
+        }
+    }
+}
